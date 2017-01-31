5a0b8310ac7beb583cad36e404d09d430a364bc3
Andreas Schultz <aschultz@tpip.net>
2017-01-31 16:54:57 +0100

    release under AGPL license, version 3
    Signed-off-by: Andreas Schultz <aschultz@tpip.net>
    Signed-off-by: Holger Winkelmann <hw@travelping.com>

--- src/dhcp_internal.h
+++ src/dhcp_internal.h
@@ -1,12 +1,18 @@
 /*
- *    __                        __      _
- *   / /__________ __   _____  / /___  (_)___  ____ _
- *  / __/ ___/ __ `/ | / / _ \/ / __ \/ / __ \/ __ `/
- * / /_/ /  / /_/ /| |/ /  __/ / /_/ / / / / / /_/ /
- * \__/_/   \__,_/ |___/\___/_/ .___/_/_/ /_/\__, /
- *                           /_/            /____/
+ * Copyright (C) 2014-2017, Travelping GmbH <info@travelping.com>
  *
- * (c) Travelping GmbH <info@travelping.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
